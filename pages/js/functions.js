1f551eec5bb06aa3d335137620753dc3996d3d71
Matheus-Gr <matheus.gr@aluno.ufop.edu.br>
2020-08-15 20:03:18 -0300

    v1.0

---
+++ pages/js/functions.js
@@ -0,0 +1,6 @@
+function toggle(){
+    var sec = document.getElementById('sec');
+    var nav = document.getElementById('navigation');
+    nav.classList.toggle('active');
+    sec.classList.toggle('active');
+}
